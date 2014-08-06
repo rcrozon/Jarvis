b3e8cd0f162e75925a13ae804dd0a874a6564825
rcrozon <crozonr@gmail.com>
2014-08-06 22:39:03 +0200

    Ajout de la gestion du volume

--- src/com/example/jarvis/ActionDetailFragment.java
+++ src/com/example/jarvis/ActionDetailFragment.java
@@ -18,6 +18,7 @@ import android.widget.Button;
 import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.SeekBar;
 
 /**
  * A fragment representing a single Action detail screen.
@@ -31,9 +32,15 @@ public class ActionDetailFragment extends Fragment implements OnClickListener {
      * represents.
      */
     public static final String ARG_ITEM_ID = "item_id";
+    private static final int STEP_VOLUME = 10;
 
-	private Button btnSendToPC;
-	private Button btnSendToTV;
+	private Button 	btnSendToPC;
+	private Button 	btnSendToTV;
+	private Button 	btnMute;
+	private Button 	btnLowVolume;
+	private Button 	btnHighVolume;
+	private SeekBar seekBarVolume;
+	private boolean muted = false;
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
      * fragment (e.g. upon screen orientation changes).
@@ -55,15 +62,21 @@ public class ActionDetailFragment extends Fragment implements OnClickListener {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-    	Log.i("LIST", "oncreateview");
         View rootView = inflater.inflate(R.layout.fragment_action_detail, container, false);
 //        return inflater.inflate(R.layout.layout_gallery, container, false);
         // Show the dummy content as text in a TextView.
 
 		btnSendToPC = (Button) rootView.findViewById(R.id.buttonPCItem);
 		btnSendToTV = (Button) rootView.findViewById(R.id.buttonTVItem);
+		btnMute = (Button) rootView.findViewById(R.id.buttonRingerOff);
+		btnLowVolume = (Button) rootView.findViewById(R.id.buttonLowVolume);
+		btnHighVolume = (Button) rootView.findViewById(R.id.buttonHighVolume);
 		btnSendToPC.setOnClickListener(this);
 		btnSendToTV.setOnClickListener(this);
+		btnMute.setOnClickListener(this);
+		btnLowVolume.setOnClickListener(this);
+		btnHighVolume.setOnClickListener(this);
+		seekBarVolume = (SeekBar)rootView.findViewById(R.id.seekBarVolume);
     	GridView vwGrid = (GridView) rootView.findViewById(R.id.gridView);
     	LinearLayout linearLayout = (LinearLayout)rootView.findViewById(R.id.linearLayout);
     	if (linearLayout != null){
@@ -75,14 +88,9 @@ public class ActionDetailFragment extends Fragment implements OnClickListener {
         		lItems.add(new Detail(3, "file 3"));
         		lItems.add(new Detail(4, "file 4"));
         		vwItems.setAdapter(new DetailAdapter(this.getActivity(), R.layout.detail, lItems));
-    		}else{
-        		Log.i("LIST", "list null");
-        	}
-    	}else{
-    		Log.i("LIST", "layout  null");
+    		}
     	}
     	if (vwGrid != null){
-    		Log.i("LIST", "grid pas null");
     		List<GalleryItem> lGalleryItems = new ArrayList<GalleryItem>();
     		lGalleryItems.add(new GalleryItem(1, "AAAA"));
     		lGalleryItems.add(new GalleryItem(2, "BBBB"));
@@ -90,22 +98,28 @@ public class ActionDetailFragment extends Fragment implements OnClickListener {
     		lGalleryItems.add(new GalleryItem(4, "DDDD"));
     		lGalleryItems.add(new GalleryItem(5, "EEEE"));
     		vwGrid.setAdapter(new GalleryAdapter(this.getActivity(), R.layout.gallery_item, lGalleryItems));
-    	}else{
-    		Log.i("LIST", "grid null");
     	}
         return rootView;
     }
 
 	@Override
 	public void onClick(View v) {
-		Log.i("CLICK", "passe");
 		if (v == btnSendToPC) {
 			btnSendToPC.setBackgroundResource(R.drawable.send_to_pc_selected);
 			btnSendToTV.setBackgroundResource(R.drawable.send_to_tv);
-		} else {
+		} else if (v == btnSendToTV){
 			btnSendToTV.setBackgroundResource(R.drawable.send_to_tv_selected);
 			btnSendToPC.setBackgroundResource(R.drawable.send_to_pc);
+		} else if (v == btnMute){
+			muted = !muted;
+			if (muted)
+				btnMute.setBackgroundResource(R.drawable.ic_lock_ringer_off_selected);
+			else
+				btnMute.setBackgroundResource(R.drawable.ic_lock_ringer_off);
+		}else if (v == btnLowVolume){
+			seekBarVolume.setProgress(seekBarVolume.getProgress() - STEP_VOLUME);
+		}else if (v == btnHighVolume){
+			seekBarVolume.setProgress(seekBarVolume.getProgress() + STEP_VOLUME);
 		}
 	}
-
 }
